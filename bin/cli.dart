2594662b8364cac39c3b57a9ee03c7466dfc90dd
isaakeil <issacneedscoffee@gmail.com>
2026-05-14 14:08:40 +0530

    Implemented Search feature

--- bin/cli.dart
+++ bin/cli.dart
@@ -2,12 +2,12 @@ const version = '0.0.1';
 const name = 'Manish';
 
 void main(List<String> arguments) {
-  if(arguments.isEmpty) {
-    print('Hello, Dart!');
+  if(arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
-  } else if (arguments.first == 'name') {
-    print('My name is $name');
+  } else if (arguments.first == 'search') {
+    print('Search command recognized');
   } else {
     print('Unknown command: ${arguments.first}');
     printUsage();
